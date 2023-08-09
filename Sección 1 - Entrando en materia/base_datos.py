a903cfde955bfbdb00f5f65614eef62e1f8a7743
KevinGiovani <66758081+KevinGiovani@users.noreply.github.com>
2023-08-08 19:29:17 -0700

    Clase #3: Inserción de datos

--- Sección 1 - Entrando en materia/base_datos.py
+++ Sección 1 - Entrando en materia/base_datos.py
@@ -1,17 +1,24 @@
 import sqlite3
 
-def conectar():
+def insertar_datos(conexion, cursor, datos):
     """
-    Conexión a la base de datos, en caso de que no existe esta se 
-    crea automaticamente
-
-    Returns:
-        list: Conexión a la BDD, cursor para operaciones
+    Inserción de datos dentro de la BDD
+    Args:
+        conexion (sqlite3.Connection): Conexión a la BDD
+        cursor (sqlite3.Cursor): Cursor para operaciones
+        datos (_type_): Valores a agregar usando la sentencia
     """
-    conexion = sqlite3.connect('pruebas.db')
-    # conexion = sqlite3.connect(':memory') # Con ':memory' la BDD se crea en memoria de modo que una vez que finalice el programa se perderan los datos
-    cursor = conexion.cursor()
-    return conexion, cursor
+    # Diferentes formas de realizar la inserción de datos en la tabla
+    # sentencia =  " INSERT INTO usuarios (usuario, email, clave) VALUES ("Rodrigo", "Rodrigo@gmail.com", "ROD44") "
+    # sentencia = "INSERT INTO usuarios (usuario, email, clave) VALUES (?, ?, ?)"
+    sentencia = "INSERT INTO usuarios VALUES (NULL,?, ?, ?)"
+    # cursor.execute(sentencia, datos) # Insertar un dato
+    cursor.executemany(sentencia,datos) # Insertar varios datos
+    #cursor.execute(sentencia)
+    print("Datos insertados correctamente")
+    conexion.commit() # Guardar los cambios
+    conexion.close()
+    
 
 def crear_tabla(conexion,cursor):
     """
@@ -33,8 +40,26 @@ def crear_tabla(conexion,cursor):
     conexion.close()
     print("Tabla creada correctamente")
 
+def conectar():
+    """
+    Conexión a la base de datos, en caso de que no existe esta se 
+    crea automaticamente
+
+    Returns:
+        list: Conexión a la BDD, cursor para operaciones
+    """
+    conexion = sqlite3.connect('pruebas.db')
+    # conexion = sqlite3.connect(':memory') # Con ':memory' la BDD se crea en memoria de modo que una vez que finalice el programa se perderan los datos
+    cursor = conexion.cursor()
+    return conexion, cursor
+
 if __name__ == '__main__':
     conexion, cursor = conectar()
-    print(type(conexion))
-    print(type(cursor))
-    crear_tabla(conexion,cursor)
+    # crear_tabla(conexion,cursor)
+    #insertar_datos(conexion, cursor)
+    #datos = ('Alan', 'alan@gmail.com', 'al2100P') # Insertar 1 dato
+    # Insertar varios datos:
+    datos = [('Yolanda', 'yolanda@gmail.com', 'y0l4A'),
+             ('Esteban','esteban@gmail.com', 'eSt001'),
+             ('Andrea','andrea@gmail.com', 'And550B')]
+    insertar_datos(conexion, cursor, datos)
