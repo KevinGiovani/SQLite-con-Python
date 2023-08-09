d37cab670c5ef111eca2a6a219c723d154c342ac
KevinGiovani <66758081+KevinGiovani@users.noreply.github.com>
2023-08-08 19:07:58 -0700

    Clase #2: Creación de tabla dentro de la BDD

--- Sección 1 - Entrando en materia/base_datos.py
+++ Sección 1 - Entrando en materia/base_datos.py
@@ -13,5 +13,28 @@ def conectar():
     cursor = conexion.cursor()
     return conexion, cursor
 
+def crear_tabla(conexion,cursor):
+    """
+    Creación de una tabla en caso de que no exista dentro de la base de datos
+    a partir de la ejecución de una sentencia de tipo 'create table'
+
+    Args:
+        conexion (sqlite3.Connection): Conexión a la BDD
+        cursor (sqlite3.Cursor): Cursor para operaciones
+    """
+    sentencia = """
+        CREATE TABLE IF NOT EXISTS usuarios
+        (id INTEGER PRIMARY KEY NOT NULL,
+        usuario TEXT NOT NULL,
+        email TEXT NOT NULL,
+        clave TEXT NOT NULL)
+    """
+    cursor.execute(sentencia)
+    conexion.close()
+    print("Tabla creada correctamente")
+
 if __name__ == '__main__':
-    con, cursor = conectar()
+    conexion, cursor = conectar()
+    print(type(conexion))
+    print(type(cursor))
+    crear_tabla(conexion,cursor)
